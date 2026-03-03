fa9de62f798c663a7fbeb20eb706b6643d60d79c
matheusbd-dev <matheusbezerradomingos@gmail.com>
2026-03-03 10:54:43 -0300

    Primeiras Configurações

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/*#!/usr/bin/env node
+console.log(
+  "Hello World",
+);  Fala ao sistema que quando for executador uso o interpretador node
+
+/*"bin": {
+    "CLI_task_Tracker": "./index.js"  Transformando esse indice em um executavel
+},*/
+console.log("Hello World");
